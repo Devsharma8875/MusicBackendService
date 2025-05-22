d43781cbc44212b223a70ce8ae5de928eaa0fb10
DEVSHARMA <dev02930@gmail.com>
2025-05-22 16:56:24 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const ytdl = require("@distube/ytdl-core");
+const cors = require("cors");
+app.use(cors());
 
 app.get("/song/:id", async (req, res) => {
   try {
